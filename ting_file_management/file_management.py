525da91ce355c4ff6308fe4f8b56fcc88f108d44
Gustavo Sant'Ana <98183666+GustavoAnatnas@users.noreply.github.com>
2023-03-13 19:22:29 +0000

    Implementa função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file_extension = os.path.splitext(path_file)[1]
+
+    if file_extension != '.txt':
+        print("Formato inválido", file=sys.stderr)
+        return []
+   
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+    with open(path_file, 'r') as file:
+        lines = [line.strip() for line in file]
+        return lines
+    
+
