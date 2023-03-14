9def1f7fe0e4d400569e6ecf84bb913e41b3d39c
Gustavo Sant'Ana <98183666+GustavoAnatnas@users.noreply.github.com>
2023-03-14 21:09:27 +0000

    Implementa funções no módulo word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    for item in instance.get_queue():
+        found_occurrences = []
+        for line_number, line in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                found_occurrences.append({"linha": line_number})
+
+        if found_occurrences:
+            occurrences.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": found_occurrences
+            })
+
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    for item in instance.get_queue():
+        found_occurrences = []
+        for j, line in enumerate(item["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                found_occurrences.append({"linha": j, "conteudo": line})
+
+        if found_occurrences:
+            occurrences.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": found_occurrences
+            })
+
+    return occurrences
